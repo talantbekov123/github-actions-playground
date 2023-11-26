9a4cc4398fa56da1ce1ac73d725e959d2125abc5
Kairat Talantbekov <kairattalantbekov@Kairats-MacBook-Pro.local>
2023-11-27 04:24:12 +0600

    minor change

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // test commit
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
